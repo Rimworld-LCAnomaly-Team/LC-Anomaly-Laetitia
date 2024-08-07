1cb039b22cce294b98ccbb859c985e20a01c3463
DarthCY-K <452710557@qq.com>
2024-08-07 22:38:13 +0800

    add：工作时播放蕾蒂希娅八音盒音乐

---
+++ Source/Laetitia/Def/SoundDefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Laetitia.Def
+{
+    [DefOf]
+    public class SoundDefOf
+    {
+        public static SoundDef Laetitia_loop;
+    }
+}
--- Source/Laetitia/Things/LC_LaetitiaPawn.cs
+++ Source/Laetitia/Things/LC_LaetitiaPawn.cs
@@ -1,9 +1,12 @@
 ﻿using LCAnomalyLibrary.Comp;
+using Verse.Sound;
 
 namespace Laetitia.Things
 {
     public class LC_LaetitiaPawn : LC_EntityBasePawn
     {
+        protected Sustainer sustainer;
+
         public LC_LaetitiaPawn()
         {
         }
