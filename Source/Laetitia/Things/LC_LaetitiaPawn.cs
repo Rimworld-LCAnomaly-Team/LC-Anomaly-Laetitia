75bb6179461de6953d0e38969df841b89b8d08fd
DarthCY-K <452710557@qq.com>
2024-08-08 10:07:05 +0800

    add：添加小女孩的朋友

--- Source/Laetitia/Things/LC_LaetitiaPawn.cs
+++ Source/Laetitia/Things/LC_LaetitiaPawn.cs
@@ -5,8 +5,6 @@ namespace Laetitia.Things
 {
     public class LC_LaetitiaPawn : LC_EntityBasePawn
     {
-        protected Sustainer sustainer;
-
         public LC_LaetitiaPawn()
         {
         }
